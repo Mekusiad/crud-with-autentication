967c41c86da08cbfd279083305fafb584eca400e
Mauro Daisuke <mauro.daisuke@hotmail.com>
2023-12-12 15:55:35 -0400

    const{_id} alterado para const {id}

--- controllers/getUserController.js
+++ controllers/getUserController.js
@@ -2,9 +2,9 @@ import { User } from "../models/User.js";
 
 export const getUserController = async (req, res) => {
   try {
-    const { _id } = req.params;
+    const { id } = req.params;
 
-    const user = await User.find({ _id: _id }, "-password -__v");
+    const user = await User.find({ _id: id }, "-password -__v");
 
     return res.status(200).json({ user });
   } catch (error) {
