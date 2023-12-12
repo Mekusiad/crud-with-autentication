60ef75c8040303332bdb8f92c9f80d5868c9c5b6
Mauro Daisuke <mauro.daisuke@hotmail.com>
2023-12-12 15:50:07 -0400

    Implementado a rota para alterar dados de usuário por ID

--- controllers/putUserController.js
+++ controllers/putUserController.js
@@ -1,17 +1,14 @@
 import { User } from "../models/User.js";
 
 export const putUserController = async (req, res) => {
-  const { _id, name, email, phone } = req.body;
-
-  // if (!name || !email || !phone)
-  //   return res.status(400).json({ message: "Missing name or email or phone" });
+  const { id, name, email, phone } = req.body;
 
   try {
-    // if (name) req.user.name = name;
-    // if (email) req.user.email = email;
-    // if (phone) req.user.phone = phone;
+    const user = await User.updateOne(
+      { _id: res.user.id },
+      { $set: { email: email } }
+    );
 
-    const user = await User.updateOne({ _id: _id }, { $set: { email: email } });
     return res
       .status(200)
       .json({ message: "Usuário atualizado com sucesso!!" });
--- routes/putUser.js
+++ routes/putUser.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { putUserController } from "../controllers/putUserController.js";
 import { userIdMiddleware } from "../middlewares/userIdMiddleware.js";
+import { userEmailMiddleware } from "../middlewares/userEmailMiddleware.js";
 
 export const putUser = express.Router();
 
-putUser.put("/:id", userIdMiddleware, putUserController);
+putUser.put("/:id", userIdMiddleware, userEmailMiddleware, putUserController);
